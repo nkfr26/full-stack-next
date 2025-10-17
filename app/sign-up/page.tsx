25cb24cc6e6008bb84da0c229d9cc2238b60298f
nkfr26 <n@rfu.jp>
2025-10-18 03:48:12 +0900

    useSortedClasses, noUncheckedIndexedAccess

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,7 +2,7 @@ import { SignUpForm } from "./_components/sign-up-form";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <SignUpForm />
     </div>
   );
